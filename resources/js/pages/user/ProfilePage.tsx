2593006c77da26a32ead5c50b0cdc4643d170c99
MakdumIbrohim <makdumibrohim3@gmail.com>
2026-07-28 09:06:54 +0700

    fix: import Loader2 dari lucide-react di ProfilePage

--- resources/js/pages/user/ProfilePage.tsx
+++ resources/js/pages/user/ProfilePage.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Loader2 } from 'lucide-react';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../../store';
 import { setCredentials } from '../../features/auth/authSlice';
